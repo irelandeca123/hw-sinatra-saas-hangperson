2fa9c1f0a7f079788fb3cb168c6d35dc8674ec04
EC2 Default User <ec2-user@ip-172-31-42-121.us-east-2.compute.internal>
2019-02-05 21:01:19 +0000

    win/lose

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra/flash'
 require './lib/hangperson_game.rb'
 
 class HangpersonApp < Sinatra::Base
-enable :sessions
+  enable :sessions
   register Sinatra::Flash
   
   before do
@@ -28,6 +28,7 @@ enable :sessions
     # NOTE: don't change next line - it's needed by autograder!
     word = params[:word] || HangpersonGame.get_random_word
     # NOTE: don't change previous line - it's needed by autograder!
+
     @game = HangpersonGame.new(word)
     redirect '/show'
   end
@@ -36,27 +37,18 @@ enable :sessions
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
-    letter = params[:guess].to_s[0] || ''
-    
-    # handle argument errors too
+    letter = params[:guess].to_s[0]
+    ### YOUR CODE HERE ###
+    result = nil
     begin
-      if !@game.guess(letter)
-        flash[:message] = "You have already used that letter."
-      end
-      if @game.check_win_or_lose == :win
-        redirect '/win'
-      elsif @game.check_win_or_lose == :lose
-        redirect '/lose'
-      else 
-        redirect '/show'
-      end
-    rescue ArgumentError
-      flash[:message] = "Invalid guess."
-      redirect '/show'
+    result = @game.guess(letter)
+    rescue ArgumentError => e
+        flash[:message] = "Invalid guess."
     end
-    
-    
-    
+    if !result.nil? and !result
+      flash[:message] = "You have already used that letter."
+    end
+    redirect '/show'
   end
   
   # Everytime a guess is made, we should eventually end up at this route.
@@ -66,17 +58,35 @@ enable :sessions
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      session[:state] = :win
+      redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+    session[:state] = :lose
+      redirect '/lose'
+    else
+      erb :show # You may change/remove this line
+    end
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    if session[:state].nil? #stop the user from going to the /win page directly without playing the game
+      redirect '/show'
+    else
+      session.delete(:state)
+      erb :win # You may change/remove this line
+    end
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if session[:state].nil? #stop the user from going to the /lose page directly without playing the game
+      redirect '/show'
+    else
+      session.delete(:state)
+      erb :lose # You may change/remove this line
+    end
   end
   
 end
